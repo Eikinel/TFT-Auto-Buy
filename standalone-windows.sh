9623c50ba852b077a9dbca6a409b1bd4e49280cf
Eikinel <valentin.pierrel@epitech.eu>
2019-07-29 23:24:19 +0200

    wip: Pyinstaller docker builds windows exe

---
+++ standalone-windows.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+SCRIPT='ocr_tft.py'
+NAME='Windows-TFT-Auto-Buy'
+OWNER='eikinel'
+SRC_DIR='dist'
+DEST_DIR='Standalone'
+
+sudo docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows:python3 "pyinstaller --onefile $SCRIPT --name=$NAME";
+
+#sudo chown $OWNER:$OWNER dist/$NAME;
+#echo "Owner set to $OWNER:$OWNER"
+
+#sudo mv $SRC_DIR/$NAME $DEST_DIR;
+#echo "Moved .exe file from $SRC_DIR/ to $DEST_DIR/"
+
+#sudo rm -rf $SRC_DIR/ build/ __pycache__/ $NAME.spec;
+#echo "Cleared unecessary folders & files"
